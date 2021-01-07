1835ed765ca8d6ec15695a152bb3f576b460db5e
Simeon Carstens <simeon.carstens@tweag.io>
2021-01-07 19:34:26 +0100

    Downgrade Python 3.9 -> 3.8 in Nix shell
    This is to match the PYthon version in the RESAAS Nix shell

--- shell.nix
+++ shell.nix
@@ -1,12 +1,11 @@
 { pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz) {} }:
 with pkgs;
-with pkgs.python39Packages;
+with pkgs.python38Packages;
 let 
   rexfw = buildPythonPackage rec {
     pname = "rexfw";
     version = "0.1.0";
     src = pkgs.lib.cleanSource ./.;
-    doCheck = true;
     installCheckPhase = ''
       cd rexfw/test && python run_tests.py
     '';
