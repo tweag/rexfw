72a83fa4268d80f03e4b1db592cd93db362be0c1
Simeon Carstens <simeon.carstens@tweag.io>
2021-01-11 15:46:29 +0100

    Refactoring

--- rexfw/statistics/writers/http.py
+++ rexfw/statistics/writers/http.py
@@ -20,6 +20,8 @@ class LoggingError(Exception):
 
 
 class AbstractHTTPStatisticsWriter(AbstractStatisticsWriter):
+    # prefix the data key in the JSON to be sent with
+    # something informative in subclasses
     _data_prefix = ""
     def __init__(self, http_endpoint, variables_to_write=[],
                  quantities_to_write=[]):
@@ -41,8 +43,6 @@ class AbstractHTTPStatisticsWriter(AbstractStatisticsWriter):
         super().__init__(variables_to_write, quantities_to_write)
         self._http_endpoint = http_endpoint
 
-
-    @abstractmethod
     def _quantities_to_JSONable_dict(self, quantity_name, quantities):
         '''
         Turns a list of :class:`.Quantity objects into some Python object that
@@ -57,27 +57,28 @@ class AbstractHTTPStatisticsWriter(AbstractStatisticsWriter):
         return {quantity_name: value_list}
 
     @abstractmethod
+    def _sanitize_value(self, value):
+        '''
+        Sanitize a quantity's current value, e.g., for an acceptance
+        rate, replace None by 0.0
+
+        :param value: a quantity's current value
+        :type value: could be literally anything
+        '''
+        pass
+    
+    @abstractmethod
     def _sort_quantities(self, quantities):
         pass
 
-    def _sanitize_value(self, value):
+    def _make_data_dict(self, elements):
         '''
-        TODO: this might need to be customizable, e.g., to replace
-        None value for an acceptance rate by 0.0
-        '''
-        return value
-    
-    def write(self, step, elements):
-        '''
-        Sends values of quantities in elements for a given
-        step to the HTTP endpoint.
+        Make a dictionary (which can be serialized to JSON) from the data
+        in elements.
 
-        :param int step: sampling step
         :param elements: list of quantities to write
         :type elements: list of :class:`.LoggedQuantity`
         '''
-
-        metadata = {'mcmc_step': step}
         statistics_data = {}
         for quantity_name in set([quantity.name
                                   for quantity in elements]):
@@ -86,9 +87,26 @@ class AbstractHTTPStatisticsWriter(AbstractStatisticsWriter):
             data = self._quantities_to_JSONable_dict(quantity_name,
                                                      sorted_quantities)
             statistics_data.update(**data)
-        all_data = {"{}_statistics".format(self._data_prefix): statistics_data}
-        all_data.update(**metadata)
-            
+        all_data = {"{}statistics".format(self._data_prefix): statistics_data}
+
+        return all_data
+
+    def _augment_with_metadata(self, all_data, step):
+        all_data['mcmc_step'] = step
+        return all_data
+    
+    def write(self, step, elements):
+        '''
+        Sends values of quantities in elements for a given
+        step to the HTTP endpoint.
+
+        :param step: sampling step
+        :type step: int
+        :param elements: list of quantities to write
+        :type elements: list of :class:`.LoggedQuantity`
+        '''
+        all_data = self._make_data_dict(elements)
+        self._augment_with_metadata(all_data, step)
         response = requests.post(self._http_endpoint,
                                  json=json.dumps(all_data))
         if not response.ok:
@@ -101,8 +119,14 @@ class REHTTPStatisticsWriter(AbstractHTTPStatisticsWriter):
     def _sort_quantities(self, quantities):
         return sort_re_quantities(quantities)
 
+    def _sanitize_value(self, value):
+        return 0.0 if value is None else value
+
 class MCMCHTTPStatisticsWriter(AbstractHTTPStatisticsWriter):
     _data_prefix = "mcmc_"
     
     def _sort_quantities(self, quantities):
         return sort_mcmc_quantities(quantities)
+
+    def _sanitize_value(self, value):
+        return 0.0 if value is None else value
